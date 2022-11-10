6fecc26966862691ad3cdf4b5e7dbf00d7a040bd
gerizimr <sinaicisco1193@gmail.com>
2022-11-09 18:05:21 -0600

    corrigiendo operadores

---
+++ operators/arithmeticOerators.dart
@@ -0,0 +1,14 @@
+void main() {
+  int suma = 5 + 5;
+  double substraction = 5-1;
+  int multiplication = 5*5;
+  double division = 4/4;
+  double modulo = 5%3;
+
+  print(suma);
+  print(substraction);
+  print(multiplication);
+  print(division);
+  print(modulo);
+  
+}
